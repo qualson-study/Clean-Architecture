48243234de6d1cbbdea7b93ed72363b2ff7492ed
JaeYoung0 <hjy8697@gmail.com>
2022-07-05 18:38:04 +0900

    feat: 1. UI in Adapter - Page Route

--- clean-architecture/src/App.tsx
+++ clean-architecture/src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Auth from "./ui/Auth";
+import User from "./ui/User";
+import Front from "./ui/Front";
+
+// feat: 1. UI in Adapter - Page Route
 function App() {
-  return <div>reset</div>;
+  return (
+    <Router>
+      <div className='app'>
+        <Switch>
+          <Route path='/auth' component={Auth} />
+          <Route path='/user' component={User} />
+          <Route path='/' component={Front} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
