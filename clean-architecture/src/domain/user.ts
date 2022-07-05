8a1a7645f2d240961e7928d799da61911c11d8f7
JaeYoung0 <hjy8697@gmail.com>
2022-07-05 18:51:51 +0900

    feat: 2. Auth Page * import types from domain layer * wrtie form in Auth Page

--- clean-architecture/src/domain/user.ts
+++ clean-architecture/src/domain/user.ts
@@ -1,3 +1,5 @@
+// here 2. domain layer - 이 안에서 핵심 데이터 타입들을 관리
+
 export type UserName = string;
 export type User = {
   id: UniqueId;
--- clean-architecture/src/ui/Auth/Auth.tsx
+++ clean-architecture/src/ui/Auth/Auth.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import { useState } from "react";
+import { Redirect } from "react-router";
+
+import { UserName } from "@/domain/user";
 
 function Auth() {
-  return <div>Auth</div>;
+  const [name, setName] = useState<UserName>("");
+  const [email, setEmail] = useState<Email>("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    setLoading(true);
+    e.preventDefault();
+
+    // authenticate logic here
+
+    setLoading(false);
+  };
+
+  const user = {};
+
+  if (!!user) return <Redirect to='/' />;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <span>Name</span>
+        <input
+          type='text'
+          name='name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          autoFocus
+        />
+      </label>
+      <label>
+        <span>Email</span>
+        <input
+          type='email'
+          name='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+
+      <button type='submit' disabled={loading}>
+        {loading ? "Trying to login..." : "Login"}
+      </button>
+    </form>
+  );
 }
 
 export default Auth;
